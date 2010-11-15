c265d2805058634cb576a2f5acafa884e2b5b469
Uriel Corfa <uriel@corfa.fr>
2010-11-15 18:23:35 +0100

    Adds more comments to rmm.h

--- test/rmm.h
+++ test/rmm.h
@@ -36,9 +36,25 @@ struct rmm_internal {
   struct rmm_chunkinfo	chunk[1024]; // 1ki * 2B = 2kiB
 } __attribute__((packed)); // total = 2050 kiB
 
+// Initializes the RMM. This should be called once and only once, in a
+// non-paginated environment, preferably at the very start of the
+// kernel. It takes the multiboot_info as a parameter to identify free
+// regions of the RAM, and returns the total available memory.
 size_t	rmm_init(struct multiboot_info* mbi);
+
+// Allocates a single physical page and returns its address.
+// Caution : The page may not be accessible in the current paging context
+// This function panic()'s if no memory is available
 uintptr_t rmm_allocate_page(void);
+
+// Gives back a physical page to the RMM. This takes any address in
+// the page, but this may have to change some day.
+void rmm_reclaim_page(uintptr_t page);
+
+#ifdef RMM_DEBUG
+// Debug utility : dumps the memory map (as provided by Multiboot) to the screen
 void dump_memory_map(struct multiboot_info* mbi);
+#endif
 
 #define CHUNK_SIZE (1024 * PAGE_SIZE)
 
