0532d60ad0be7475ec304ab74ad949d7541a5e4c
Uriel Corfa <uriel@corfa.fr>
2010-12-15 17:55:43 +0100

    Cleanup in threads.c

--- test/threads.c
+++ test/threads.c
@@ -22,12 +22,10 @@ void thr_printf(char* format, ...) {
 #endif
 
 void schedule(void) {
-  //  threads[current_thread] = (unsigned long)esp;
   int old_thread = current_thread;
   if (max_thread == -1) {
     panic("Last thread died\n");
   }
-  thr_printf("We are at thread #%d\n", current_thread);
   int n = 0;
   do {
     current_thread++;
@@ -47,28 +45,21 @@ void schedule(void) {
 }
 
 void thread_entry(void* old_stack, void (*entry)(void* data), void* data) {
-  thr_printf("New thread ! %p We were previously in thread %d\n", entry, current_thread);
-
   threads[current_thread].esp = (unsigned long)old_stack;
   max_thread++;
   current_thread = max_thread;
   
-  thr_printf("This thread has id #%d\n", current_thread);
-
   (*entry)(data);
-  thr_printf("Out of thread %p\n", entry);
 
   threads[current_thread].esp = 0;
   threads[current_thread].pctx = 0;
   current_thread = -1;
   for (;;) schedule();
-  thr_printf("We should have switched stacks by now... at %p\n", entry);
 }
 
 void new_thread(void (*fct)(void* data), void* data) {
   unsigned long* stack = kmalloc(4000);
   new_thread_asm((unsigned long)(stack + 1000), fct, &(threads[current_thread].esp), data);
-  thr_printf("WTF at end of new_thread\n");
 }
 
 void start_threads(void (*startfunction)(void*)) {
