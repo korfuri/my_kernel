c94f833cb926d3da9b9528a9310f5ad90616728a
Uriel Corfa <uriel@corfa.fr>
2010-12-16 01:04:13 +0100

    Adds basic routines to read/write data to/from IO ports

---
+++ test/ports.h
@@ -0,0 +1,23 @@
+static unsigned char inportb (unsigned short _port)
+{
+    unsigned char rv;
+    __asm__ __volatile__ ("inb %1, %0" : "=a" (rv) : "dN" (_port));
+    return rv;
+}
+
+static void outportb (unsigned short _port, unsigned char _data)
+{
+    __asm__ __volatile__ ("outb %1, %0" : : "dN" (_port), "a" (_data));
+}
+
+static unsigned long inportl (unsigned short _port)
+{
+    unsigned long rv;
+    __asm__ __volatile__ ("inl %1, %0" : "=a" (rv) : "dN" (_port));
+    return rv;
+}
+
+static void outportl (unsigned short _port, unsigned long _data)
+{
+    __asm__ __volatile__ ("outl %1, %0" : : "dN" (_port), "a" (_data));
+}
