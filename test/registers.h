928256a2c863ca9df704fe75fb4a057e35a49ed3
Uriel Corfa <uriel@corfa.fr>
2010-12-16 01:05:27 +0100

    Adds registers.h with struct registers, mapping registers as saved by a pushad

---
+++ test/registers.h
@@ -0,0 +1,15 @@
+#ifndef REGISTERS_H_
+#define REGISTERS_H_
+
+struct registers {
+  unsigned long edi;
+  unsigned long esi;
+  unsigned long ebp;
+  unsigned long esp;
+  unsigned long ebx;
+  unsigned long edx;
+  unsigned long ecx;
+  unsigned long eax;
+};
+
+#endif
